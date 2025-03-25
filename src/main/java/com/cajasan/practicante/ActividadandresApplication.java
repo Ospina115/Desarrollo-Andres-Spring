c4157ada8667cf4c4efd58d360bc13fe7ca71ec6
Ospina115 <158192117+Ospina115@users.noreply.github.com>
2025-03-25 08:31:23 -0500

    👷 Proyect Files
    agregamos todo lo necesario para inicializar el servidor en springboot y todas las dependencias para su correcto funcionamiento en todas las aplicaciones que se nos soliciten

---
+++ src/main/java/com/cajasan/practicante/ActividadandresApplication.java
@@ -0,0 +1,13 @@
+package com.cajasan.practicante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadandresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadandresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cajasan/practicante/ActividadandresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cajasan.practicante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadandresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
